a1186f0c6a671024f3f41e48dc4e643eb1af8dd2
Akeel Ather Medina <akeelmedina22@gmail.com>
2023-02-24 21:19:43 +0500

    chapter1 + chapter2 working commit

--- 1-matrixless/Warp/Fluid.py
+++ 1-matrixless/Warp/Fluid.py
@@ -5,12 +5,22 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 
 wp.init()
-# wp.config.verify_cuda = True
 
 grid_width = wp.constant(400)
 grid_height = wp.constant(400)
-density = wp.constant(0.5)
+density = wp.constant(0.01)
 hx = wp.constant(1.0/min(grid_width.val, grid_height.val))
+
+
+@wp.struct
+class Fluid:
+    w   : int
+    h   : int
+    ox  : float
+    oy  : float
+    src : wp.array2d(dtype=float)
+    dst : wp.array2d(dtype=float)
+
 
 @wp.func
 def at(f: wp.array2d(dtype=float),
@@ -30,9 +40,9 @@ def addInflow(d_src: wp.array2d(dtype=float),
 
     i, j = wp.tid()
 
-    if i < 25 and i > 20 and j < 220 and j > 180:
+    if i < 220 and i > 180 and j < 220 and j > 180:
         if d_src[i,j] < d:
-            d_src[  i,j] = d
+            d_src[i,j] = d
         if u_src[i,j] < u:
             u_src[i,j] = u
         if v_src[i,j] < v:
@@ -40,44 +50,58 @@ def addInflow(d_src: wp.array2d(dtype=float),
 
 
 @wp.func
-def lin_grid_sample(f: wp.array2d(dtype=float), x: float, y: float):
+def lin_grid_sample(f: Fluid, x: float, y: float):
 
-    lx = int(wp.floor(x))
-    ly = int(wp.floor(y))
-
-    tx = x-float(lx)
-    ty = y-float(ly)
+    x = min(max(x - f.ox, 0.0), float(f.w) - 1.001)
+    y = min(max(y - f.oy, 0.0), float(f.h) - 1.001)
+    ix = int(x)
+    iy = int(y)
+    x = x - float(ix)
+    y = y - float(iy)
     
-    s0 = wp.lerp(at(f, lx, ly), at(f, lx+1, ly), tx)
-    s1 = wp.lerp(at(f, lx, ly+1), at(f, lx+1, ly+1), tx)
+    x00 = at(f.src, ix + 0, iy + 0)
+    x10 = at(f.src, ix + 1, iy + 0)
+    x01 = at(f.src, ix + 0, iy + 1)
+    x11 = at(f.src, ix + 1, iy + 1)
+    
+    return wp.lerp(wp.lerp(x00, x10, x), wp.lerp(x01, x11, x), y)
 
-    s = wp.lerp(s0, s1, ty)
-    return s
+
+@wp.func
+def euler(u: Fluid, v: Fluid, x: float, y: float, timestep: float):
+    uVel = lin_grid_sample(u, x, y)/hx
+    vVel = lin_grid_sample(v, x, y)/hx
+    
+    x = x - uVel*timestep
+    y = y - vVel*timestep
+
+    return wp.vec2(x, y)
 
 
 @wp.kernel
-def advect(d_src: wp.array2d(dtype=float),
-           d_dst: wp.array2d(dtype=float),
-           u_src: wp.array2d(dtype=float),
-           u_dst: wp.array2d(dtype=float),
-           v_src: wp.array2d(dtype=float),
-           v_dst: wp.array2d(dtype=float),
+def advect(d: Fluid,
+           u: Fluid,
+           v: Fluid,
            timestep: float):
 
     i,j = wp.tid()
     
-    x = float(i)
-    y = float(j)
+    ux = float(i) + u.ox
+    uy = float(j) + u.oy
 
-    uVel = lin_grid_sample(u_src, x, y)
-    vVel = lin_grid_sample(v_src, x, y)
+    vx = float(i) + v.ox
+    vy = float(j) + v.oy
 
-    x = x - uVel*timestep
-    y = y - vVel*timestep
+    dx = float(i) + d.ox
+    dy = float(j) + d.oy
 
-    d_dst[i,j] = lin_grid_sample(d_src, x, y)
-    u_dst[i,j] = lin_grid_sample(u_src, x, y)
-    v_dst[i,j] = lin_grid_sample(v_src, x, y)
+    _dxy = euler(u, v, dx, dy, timestep)
+    _uxy = euler(u, v, ux, uy, timestep)
+    _vxy = euler(u, v, vx, vy, timestep)
+
+    d.dst[i,j] = lin_grid_sample(d, _dxy[0], _dxy[1])
+    u.dst[i,j] = lin_grid_sample(u, _uxy[0], _uxy[1])
+    v.dst[i,j] = lin_grid_sample(v, _vxy[0], _vxy[1])
 
 
 @wp.kernel
@@ -117,46 +141,74 @@ def project(r: wp.array2d(dtype=float), p_src: wp.array2d(dtype=float), p_dst: w
 @wp.kernel
 def applyPressure(u: wp.array2d(dtype=float), v: wp.array2d(dtype=float), p: wp.array2d(dtype=float), timestep: float):
 
-    scale = timestep / (density * hx)
     i,j = wp.tid()
 
-    u[i,    j]    = u[i,    j]    - scale*p[i,j]
-    u[(i+1),j]    = u[(i+1),j]    + scale*p[i,j]
-    v[i,    j]    = v[i,    j]    - scale*p[i,j]
-    v[i,   (j+1)] = v[i,   (j+1)] + scale*p[i,j]
+    if i == 0 or i == grid_width-1:
+        return
+    if j == 0 or j == grid_height-1:
+        return
+    
 
-
-    if i == 0 or i == grid_width:
-        u[i, j] = 0.0
-    if j == 0 or j == grid_height:
-        v[i, j] = 0.0
+    scale = (timestep/1000.0)/(density*hx)
+    f_p = wp.vec2(p[i+1, j] - p[i-1, j],
+                  p[i, j+1] - p[i, j-1])*scale
+    u[i,j] = u[i,j] - f_p[0]
+    v[i,j] = v[i,j] - f_p[1]
 
 
 class FluidSolver:
 
     def __init__(self):
 
-        self.sim_substeps = 1
+        self.sim_substeps = 4
         self.sim_dt = 0.05
         self.sim_time = 0.0
         self.iterations = 600
 
         self.device = wp.get_device()
 
+        self.u = Fluid()
+        self.u.w = grid_width.val+1
+        self.u.h = grid_height.val
+        self.u.ox = 0.0
+        self.u.oy = 0.5
+        self.u.src = wp.zeros((self.u.w, self.u.h), dtype=float)
+        self.u.dst = wp.zeros((self.u.w, self.u.h), dtype=float)
+
+        self.v = Fluid()
+        self.v.w = grid_width.val
+        self.v.h = grid_height.val+1
+        self.v.ox = 0.5
+        self.v.oy = 0.0
+        self.v.src = wp.zeros((self.v.w, self.v.h), dtype=float)
+        self.v.dst = wp.zeros((self.v.w, self.v.h), dtype=float)
+
+        self.d = Fluid()
+        self.d.w = grid_width.val
+        self.d.h = grid_height.val
+        self.d.ox = 0.5
+        self.d.oy = 0.5
+        self.d.src = wp.zeros((self.d.w, self.d.h), dtype=float)
+        self.d.dst = wp.zeros((self.d.w, self.d.h), dtype=float)
+
         shape = (grid_width.val, grid_height.val)
-
-        self.u_src = wp.zeros(shape, dtype=float)
-        self.u_dst = wp.zeros(shape, dtype=float)
-
-        self.v_src = wp.zeros(shape, dtype=float)
-        self.v_dst = wp.zeros(shape, dtype=float)
-
-        self.d_src = wp.zeros(shape, dtype=float)
-        self.d_dst = wp.zeros(shape, dtype=float)
-
         self.r = wp.zeros(shape, dtype=float)
         self.p_src = wp.zeros(shape, dtype=float)
         self.p_dst = wp.zeros(shape, dtype=float)
+
+        # capture pressure solve as a CUDA graph
+        if self.device.is_cuda:
+            wp.capture_begin()
+            self.solve()
+            self.graph = wp.capture_end()
+
+
+    def solve(self):
+
+        for j in range(self.iterations):
+            wp.launch(project, dim=(grid_width.val, grid_height.val), inputs=[self.r, self.p_src, self.p_dst, self.sim_dt])
+            (self.p_src, self.p_dst) = (self.p_dst, self.p_src)
+
 
 
     def render(self, img, i):
@@ -165,28 +217,32 @@ class FluidSolver:
         
             shape = (grid_width.val, grid_height.val)
             dt = self.sim_dt
-            
+
             # update emitters
-            wp.launch(addInflow, dim=shape, inputs=[self.d_src, self.u_src, self.v_src, 1.0, 0.0, 3.0])
+            wp.launch(addInflow, dim=shape, inputs=[self.d.src, self.u.src, self.v.src, 1.0, 0.0, 3.0])
 
-            wp.launch(buildRhs, dim=shape, inputs=[self.r, self.u_src, self.v_src])
+            wp.launch(buildRhs, dim=shape, inputs=[self.r, self.u.src, self.v.src])
 
-            for j in range(self.iterations):
-                wp.launch(project, dim=shape, inputs=[self.r, self.p_src, self.p_dst, dt])
-                (self.p_src, self.p_dst) = (self.p_dst, self.p_src)
+            if self.device.is_cuda:
+                wp.capture_launch(self.graph)
+            else:
+                self.solve()  
+            
+            wp.launch(applyPressure, dim=shape, inputs=[self.u.dst, self.v.dst, self.p_src, dt])
 
-            wp.launch(applyPressure, dim=shape, inputs=[self.u_src, self.v_src, self.p_src, dt])
+            (self.u.src, self.u.dst) = (self.u.dst, self.u.src)
+            (self.v.src, self.v.dst) = (self.v.dst, self.v.src)
 
             # advect
-            wp.launch(advect, dim=shape, inputs=[self.d_src, self.d_dst, self.u_src, self.u_dst, self.v_src, self.v_dst, dt])
+            wp.launch(advect, dim=shape, inputs=[self.d, self.u, self.v, dt])
 
-            (self.d_src, self.d_dst) = (self.d_dst, self.d_src)
-            (self.u_src, self.u_dst) = (self.u_dst, self.u_src)
-            (self.v_src, self.v_dst) = (self.v_dst, self.v_src)
+            (self.d.src, self.d.dst) = (self.d.dst, self.d.src)
+            (self.u.src, self.u.dst) = (self.u.dst, self.u.src)
+            (self.v.src, self.v.dst) = (self.v.dst, self.v.src)
 
             self.sim_time += dt
 
-        img.set_array(self.d_src.numpy())
+        img.set_array(self.d.src.numpy())
 
         return img,
         
@@ -197,9 +253,9 @@ if __name__ == '__main__':
 
     fig = plt.figure()
 
-    img = plt.imshow(chapter1.d_src.numpy(), origin="lower", animated=True, interpolation="antialiased")
+    img = plt.imshow(chapter1.d.src.numpy(), origin="lower", animated=True, interpolation="antialiased")
     img.set_norm(matplotlib.colors.Normalize(0.0, 1.0))
-    seq = anim.FuncAnimation(fig, lambda i: chapter1.render(img, i), frames=10000, blit=True, interval=8, repeat=False)
+    seq = anim.FuncAnimation(fig, lambda i: chapter1.render(img, i), frames=100000, blit=True, interval=8, repeat=False)
     # seq.save('chapter1.gif') 
     
     plt.show()
--- 2-better-advection/Warp/Fluid.py
+++ 2-better-advection/Warp/Fluid.py
@@ -5,23 +5,33 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 
 wp.init()
-# wp.config.verify_cuda = True
 
 grid_width = wp.constant(400)
 grid_height = wp.constant(400)
-density = wp.constant(0.5)
+density = wp.constant(0.01)
 hx = wp.constant(1.0/min(grid_width.val, grid_height.val))
+
+
+@wp.struct
+class Fluid:
+    w   : int
+    h   : int
+    ox  : float
+    oy  : float
+    src : wp.array2d(dtype=float)
+    dst : wp.array2d(dtype=float)
 
 
 @wp.func
 def at(f: wp.array2d(dtype=float),
-       x: int,
-       y: int):
+                 x: int,
+                 y: int):
 
     x = wp.clamp(x, 0, grid_width-1)
     y = wp.clamp(y, 0, grid_height-1)
 
-    return f[x, y]
+    return f[x,y]
+    
 
 @wp.func
 def length(x: float, y: float):
@@ -40,8 +50,8 @@ def addInflow(d_src: wp.array2d(dtype=float),
 
     i, j = wp.tid()
 
-    x0 = 20
-    x1 = 25
+    x0 = 180
+    x1 = 220
     y0 = 180
     y1 = 220
 
@@ -80,49 +90,52 @@ def cerp(a: float, b: float, c: float, d: float, x: float):
 
 
 @wp.func
-def cubic_grid_sample(fluid: wp.array2d(dtype=float), x: float, y: float):
-    lx = int(wp.floor(x))
-    ly = int(wp.floor(y))
+def lin_grid_sample(f: Fluid, x: float, y: float):
 
-    tx = x-float(lx)
-    ty = y-float(ly)
-
-    x0 = max(lx - 1, 0)
-    x1 = lx
-    x2 = lx + 1
-    x3 = min(lx + 2, grid_width - 1)
-
-    y0 = max(ly - 1, 0)
-    y1 = ly
-    y2 = ly + 1
-    y3 = min(ly + 2, grid_height - 1)
+    x = min(max(x - f.ox, 0.0), float(f.w) - 1.001)
+    y = min(max(y - f.oy, 0.0), float(f.h) - 1.001)
+    ix = int(x)
+    iy = int(y)
+    x = x - float(ix)
+    y = y - float(iy)
     
-    q0 = cerp(at(fluid, x0, y0), at(fluid, x1, y0), at(fluid, x2, y0), at(fluid, x3, y0), tx)
-    q1 = cerp(at(fluid, x0, y1), at(fluid, x1, y1), at(fluid, x2, y1), at(fluid, x3, y1), tx)
-    q2 = cerp(at(fluid, x0, y2), at(fluid, x1, y2), at(fluid, x2, y2), at(fluid, x3, y2), tx)
-    q3 = cerp(at(fluid, x0, y3), at(fluid, x1, y3), at(fluid, x2, y3), at(fluid, x3, y3), tx)
+    x00 = at(f.src, ix + 0, iy + 0)
+    x10 = at(f.src, ix + 1, iy + 0)
+    x01 = at(f.src, ix + 0, iy + 1)
+    x11 = at(f.src, ix + 1, iy + 1)
     
-    return cerp(q0, q1, q2, q3, ty)
+    return wp.lerp(wp.lerp(x00, x10, x), wp.lerp(x01, x11, x), y)
 
 
 @wp.func
-def lin_grid_sample(f: wp.array2d(dtype=float), x: float, y: float):
+def cubic_grid_sample(f: Fluid, x: float, y: float):
+    x = min(max(x - f.ox, 0.0), float(f.w) - 1.001)
+    y = min(max(y - f.oy, 0.0), float(f.h) - 1.001)
+    ix = int(x)
+    iy = int(y)
+    x = x - float(ix)
+    y = y - float(iy)
 
-    lx = int(wp.floor(x))
-    ly = int(wp.floor(y))
+    x0 = max(ix - 1, 0)
+    x1 = ix
+    x2 = ix + 1
+    x3 = min(ix + 2, grid_width - 1)
 
-    tx = x-float(lx)
-    ty = y-float(ly)
+    y0 = max(iy - 1, 0)
+    y1 = iy
+    y2 = iy + 1
+    y3 = min(iy + 2, grid_height - 1)
     
-    s0 = wp.lerp(at(f, lx, ly), at(f, lx+1, ly), tx)
-    s1 = wp.lerp(at(f, lx, ly+1), at(f, lx+1, ly+1), tx)
-
-    s = wp.lerp(s0, s1, ty)
-    return s
+    q0 = cerp(at(f.src, x0, y0), at(f.src, x1, y0), at(f.src, x2, y0), at(f.src, x3, y0), x)
+    q1 = cerp(at(f.src, x0, y1), at(f.src, x1, y1), at(f.src, x2, y1), at(f.src, x3, y1), x)
+    q2 = cerp(at(f.src, x0, y2), at(f.src, x1, y2), at(f.src, x2, y2), at(f.src, x3, y2), x)
+    q3 = cerp(at(f.src, x0, y3), at(f.src, x1, y3), at(f.src, x2, y3), at(f.src, x3, y3), x)
+    
+    return cerp(q0, q1, q2, q3, y)
 
 
 @wp.func
-def rungeKutta3(x: float, y: float, timestep: float, u: wp.array2d(dtype=float), v: wp.array2d(dtype=float)):
+def rungeKutta3(u: Fluid, v: Fluid, x: float, y: float, timestep: float):
     firstU = lin_grid_sample(u, x, y)/hx
     firstV = lin_grid_sample(v, x, y)/hx
 
@@ -145,144 +158,173 @@ def rungeKutta3(x: float, y: float, timestep: float, u: wp.array2d(dtype=float),
 
 
 @wp.kernel
-def advect(d_src: wp.array2d(dtype=float),
-           d_dst: wp.array2d(dtype=float),
-           u_src: wp.array2d(dtype=float),
-           u_dst: wp.array2d(dtype=float),
-           v_src: wp.array2d(dtype=float),
-           v_dst: wp.array2d(dtype=float),
-           dt: float):
-
+def advect(d: Fluid,
+           u: Fluid,
+           v: Fluid,
+           timestep: float):
+    
     i, j = wp.tid()
 
-    x = float(i)
-    y = float(j)
+    ux = float(i) + u.ox
+    uy = float(j) + u.oy
 
-    _ = wp.vec2()
+    vx = float(i) + v.ox
+    vy = float(j) + v.oy
 
-    _ = rungeKutta3(x, y, dt, u_src, v_src)
+    dx = float(i) + d.ox
+    dy = float(j) + d.oy
 
-    x = _[0]
-    y = _[1]
+    _dxy = rungeKutta3(u, v, dx, dy, timestep)
+    _uxy = rungeKutta3(u, v, ux, uy, timestep)
+    _vxy = rungeKutta3(u, v, vx, vy, timestep)
 
-    d_dst[i, j] = cubic_grid_sample(d_src, x, y)
-    u_dst[i, j] = cubic_grid_sample(u_src, x, y)
-    v_dst[i, j] = cubic_grid_sample(v_src, x, y)
+    d.dst[i,j] = cubic_grid_sample(d, _dxy[0], _dxy[1])
+    u.dst[i,j] = cubic_grid_sample(u, _uxy[0], _uxy[1])
+    v.dst[i,j] = cubic_grid_sample(v, _vxy[0], _vxy[1])
 
 
 @wp.kernel
 def buildRhs(r: wp.array2d(dtype=float), u: wp.array2d(dtype=float), v: wp.array2d(dtype=float)):
     scale = 1.0/hx
-    i, j = wp.tid()
-    r[i, j] = -scale*(at(u, i+1, j) - at(u, i, j) +
-                      at(v, i, j+1) - at(v, i, j))
-
+    i,j = wp.tid()
+    r[i,j] = -scale*(at(u, i+1, j) - at(u, i, j) +
+                            at(v, i, j+1) - at(v, i, j))
 
 @wp.kernel
 def project(r: wp.array2d(dtype=float), p_src: wp.array2d(dtype=float), p_dst: wp.array2d(dtype=float), timestep: float):
 
     scale = timestep / (density*hx*hx)
 
-    i, j = wp.tid()
+    i,j = wp.tid()
 
     diag = float(0.0)
     offDiag = float(0.0)
 
     if i > 0:
-        diag += scale
-        offDiag -= scale * at(p_src, i-1, j)
+        diag    = diag + scale
+        offDiag = offDiag - scale * at(p_src, i-1, j)
     if j > 0:
-        diag += scale
-        offDiag -= scale * at(p_src, i, j-1)
+        diag    = diag + scale
+        offDiag = offDiag - scale * at(p_src, i, j-1)
     if i < grid_width - 1:
-        diag += scale
-        offDiag -= scale * at(p_src, i+1, j)
+        diag    = diag + scale
+        offDiag = offDiag - scale * at(p_src, i+1, j)
     if j < grid_height - 1:
-        diag += scale
-        offDiag -= scale * at(p_src, i, j+1)
-
+        diag    = diag + scale
+        offDiag = offDiag - scale * at(p_src, i, j+1)
+        
     newP = (at(r, i, j) - offDiag)/diag
-    p_dst[i, j] = newP
+    p_dst[i,j] = newP
 
 
 @wp.kernel
 def applyPressure(u: wp.array2d(dtype=float), v: wp.array2d(dtype=float), p: wp.array2d(dtype=float), timestep: float):
 
-    scale = timestep / (density * hx)
-    i, j = wp.tid()
+    scale = (timestep/1000.0)/(density*hx)
+    i,j = wp.tid()
 
-    u[i, j] = u[i, j] - scale*p[i, j]
-    u[(i+1), j] = u[(i+1), j] + scale*p[i, j]
-    v[i, j] = v[i, j] - scale*p[i, j]
-    v[i, (j+1)] = v[i, (j+1)] + scale*p[i, j]
+    if i == 0 or i == grid_width-1:
+        return
+    if j == 0 or j == grid_height-1:
+        return
+    
 
-    if i == 0 or i == grid_width:
-        u[i, j] = 0.0
-    if j == 0 or j == grid_height:
-        v[i, j] = 0.0
+    f_p = wp.vec2(p[i+1, j] - p[i-1, j],
+                  p[i, j+1] - p[i, j-1])*scale
+    u[i,j] = u[i,j] - f_p[0]
+    v[i,j] = v[i,j] - f_p[1]
 
 
 class FluidSolver:
 
     def __init__(self):
 
-        self.sim_substeps = 1
+        self.sim_substeps = 4
         self.sim_dt = 0.05
         self.sim_time = 0.0
         self.iterations = 600
 
         self.device = wp.get_device()
 
+        self.u = Fluid()
+        self.u.w = grid_width.val+1
+        self.u.h = grid_height.val
+        self.u.ox = 0.0
+        self.u.oy = 0.5
+        self.u.src = wp.zeros((self.u.w, self.u.h), dtype=float)
+        self.u.dst = wp.zeros((self.u.w, self.u.h), dtype=float)
+
+        self.v = Fluid()
+        self.v.w = grid_width.val
+        self.v.h = grid_height.val+1
+        self.v.ox = 0.5
+        self.v.oy = 0.0
+        self.v.src = wp.zeros((self.v.w, self.v.h), dtype=float)
+        self.v.dst = wp.zeros((self.v.w, self.v.h), dtype=float)
+
+        self.d = Fluid()
+        self.d.w = grid_width.val
+        self.d.h = grid_height.val
+        self.d.ox = 0.5
+        self.d.oy = 0.5
+        self.d.src = wp.zeros((self.d.w, self.d.h), dtype=float)
+        self.d.dst = wp.zeros((self.d.w, self.d.h), dtype=float)
+
         shape = (grid_width.val, grid_height.val)
-
-        self.u_src = wp.zeros(shape, dtype=float)
-        self.u_dst = wp.zeros(shape, dtype=float)
-
-        self.v_src = wp.zeros(shape, dtype=float)
-        self.v_dst = wp.zeros(shape, dtype=float)
-
-        self.d_src = wp.zeros(shape, dtype=float)
-        self.d_dst = wp.zeros(shape, dtype=float)
-
         self.r = wp.zeros(shape, dtype=float)
         self.p_src = wp.zeros(shape, dtype=float)
         self.p_dst = wp.zeros(shape, dtype=float)
 
+        # capture pressure solve as a CUDA graph
+        if self.device.is_cuda:
+            wp.capture_begin()
+            self.solve()
+            self.graph = wp.capture_end()
+
+
+    def solve(self):
+
+        for j in range(self.iterations):
+            wp.launch(project, dim=(grid_width.val, grid_height.val), inputs=[self.r, self.p_src, self.p_dst, self.sim_dt])
+            (self.p_src, self.p_dst) = (self.p_dst, self.p_src)
+
+
+
     def render(self, img, i):
 
         for i in range(self.sim_substeps):
-
+        
             shape = (grid_width.val, grid_height.val)
             dt = self.sim_dt
 
             # update emitters
-            wp.launch(addInflow, dim=shape, inputs=[
-                      self.d_src, self.u_src, self.v_src, 1.0, 0.0, 3.0])
+            wp.launch(addInflow, dim=shape, inputs=[self.d.src, self.u.src, self.v.src, 1.0, 0.0, 3.0])
 
-            wp.launch(buildRhs, dim=shape, inputs=[
-                      self.r, self.u_src, self.v_src])
+            wp.launch(buildRhs, dim=shape, inputs=[self.r, self.u.src, self.v.src])
 
-            for j in range(self.iterations):
-                wp.launch(project, dim=shape, inputs=[self.r, self.p_src, self.p_dst, dt])
-                (self.p_src, self.p_dst) = (self.p_dst, self.p_src)
+            if self.device.is_cuda:
+                wp.capture_launch(self.graph)
+            else:
+                self.solve()  
+            
+            wp.launch(applyPressure, dim=shape, inputs=[self.u.dst, self.v.dst, self.p_src, dt])
 
-            wp.launch(applyPressure, dim=shape, inputs=[
-                      self.u_src, self.v_src, self.p_src, dt])
+            (self.u.src, self.u.dst) = (self.u.dst, self.u.src)
+            (self.v.src, self.v.dst) = (self.v.dst, self.v.src)
 
             # advect
-            wp.launch(advect, dim=shape, inputs=[
-                      self.d_src, self.d_dst, self.u_src, self.u_dst, self.v_src, self.v_dst, dt])
+            wp.launch(advect, dim=shape, inputs=[self.d, self.u, self.v, dt])
 
-            (self.d_src, self.d_dst) = (self.d_dst, self.d_src)
-            (self.u_src, self.u_dst) = (self.u_dst, self.u_src)
-            (self.v_src, self.v_dst) = (self.v_dst, self.v_src)
+            (self.d.src, self.d.dst) = (self.d.dst, self.d.src)
+            (self.u.src, self.u.dst) = (self.u.dst, self.u.src)
+            (self.v.src, self.v.dst) = (self.v.dst, self.v.src)
 
             self.sim_time += dt
 
-        img.set_array(self.d_src.numpy())
+        img.set_array(self.d.src.numpy())
 
         return img,
-
+        
 
 if __name__ == '__main__':
 
@@ -290,7 +332,7 @@ if __name__ == '__main__':
 
     fig = plt.figure()
 
-    img = plt.imshow(chapter2.d_src.numpy(), origin="lower", animated=True, interpolation="antialiased")
+    img = plt.imshow(chapter2.d.src.numpy(), origin="lower", animated=True, interpolation="antialiased")
     img.set_norm(matplotlib.colors.Normalize(0.0, 1.0))
     seq = anim.FuncAnimation(fig, lambda i: chapter2.render(img, i), frames=10000, blit=True, interval=8, repeat=False)
     # seq.save('chapter2.gif') 
